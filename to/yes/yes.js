7e11e0abe485ae5ca736a5fc5cfd12c4bac1e873
Akmal Ramdana <154916120+akmalrdn@users.noreply.github.com>
2024-01-07 11:26:09 +0700

    Add files via upload

---
+++ to/yes/yes.js
@@ -0,0 +1,72 @@
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+       console.log(entry)
+       if (entry.isIntersecting) {
+         entry.target.classList.add('show');
+         } else {
+           entry.target.classList.remove('show');
+         }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el)); 
+
+
+var swiper3 = new Swiper(".mySwiper3", {
+      grabCursor: true,
+      effect: "creative",
+      creativeEffect: {
+        prev: {
+          shadow: true,
+          translate: ["-20%", 0, -1],
+        },
+        next: {
+          translate: ["100%", 0, 0],
+        },
+      },
+    });
+    
+   
